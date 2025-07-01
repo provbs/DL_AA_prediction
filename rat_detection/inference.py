7dba942a8062a49c5bae938994aeb05bf04bb14a
Youngjae Kim <97097656+provbs@users.noreply.github.com>
2025-07-01 11:50:51 +0900

    Update inference.py

--- rat_detection/inference.py
+++ rat_detection/inference.py
@@ -7,10 +7,8 @@ import json
 import numpy as np
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-# CustomDataset, transform을 정의한 모듈을 import합니다.
 from dataset import transform
 
-# 박스를 그리는 함수 정의
 def draw_boxes(image, boxes, scores):
     draw = ImageDraw.Draw(image)
     for box, score in zip(boxes, scores):
@@ -19,7 +17,6 @@ def draw_boxes(image, boxes, scores):
         draw.text((x1, y1), f"{score:.2f}", fill="red")
     return image
 
-# 모델 로드 함수 정의
 def load_model(model_path, device):
     model = fasterrcnn_resnet50_fpn(pretrained=False)
     num_classes = 2  # 배경 클래스 + 객체 클래스
@@ -30,11 +27,9 @@ def load_model(model_path, device):
     model.eval()
     return model
 
-# inference 함수 수정
 def inference(model, input_dir, output_dir, device):
     os.makedirs(output_dir, exist_ok=True)
     
-    # 모든 하위 폴더를 탐색하며 이미지 파일 찾기
     image_files = []
     for root, _, files in os.walk(input_dir):
         for file in files:
@@ -42,7 +37,6 @@ def inference(model, input_dir, output_dir, device):
                 image_files.append(os.path.join(root, file))
     
     for image_path in image_files:
-        # 이미지 경로에서 파일 이름 추출
         image_file = os.path.basename(image_path)
         
         # 이미지 열기 및 변환
@@ -56,7 +50,6 @@ def inference(model, input_dir, output_dir, device):
         scores = output['scores'].cpu().numpy()
         image_with_boxes = draw_boxes(image, boxes, scores)
         
-        # 결과 이미지를 출력 폴더 구조에 맞춰 저장
         relative_path = os.path.relpath(image_path, input_dir)
         output_image_path = os.path.join(output_dir, f"result_{relative_path}")
         os.makedirs(os.path.dirname(output_image_path), exist_ok=True)
@@ -76,9 +69,9 @@ def inference(model, input_dir, output_dir, device):
             json.dump(results, f, indent=4)
 
 
-# 사용 예시
-input_dir =  "/workspace/ds_Toe_off_angle/data/testset_with_label(final)/frames_with_labels"
-output_dir =   "/workspace/ds_Toe_off_angle/data/testset_with_label(final)/frames_with_labels_detection"
+
+input_dir =  ""
+output_dir =   ""
 
 model_path = 'saved_models/model_epoch_19.pth'
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
